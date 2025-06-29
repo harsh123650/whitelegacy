2ec365ebaf69e10289c88c791c9ebad0d641aedc
Harshal <harshal123@gmail.com>
2025-06-29 19:13:39 +0530

    Final commit with subscription_select.html and other pending files

--- milkapp_project/settings.py
+++ milkapp_project/settings.py
@@ -33,7 +33,8 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 
